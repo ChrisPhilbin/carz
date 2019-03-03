cfe6bbb41bc8cb2d0ca9562cce43f00cedcb483f
Chris Philbin <chris@chrisphilbin.net>
2019-03-02 19:09:50 -0500

    Working on developing the functionality for each view

--- app.rb
+++ app.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
+require_relative './config/environment'
 
 class Carz < Sinatra::Base
 
 	get '/' do
-		"Hello, world!"
+		@cars = Car.all
+		erb :index
 	end
 
-	get '/:name' do
-		"Hello #{params[:name]}"
-	end
+	get '/cars/:id' do
+		@car = Car.find_by(id: params[:id]) #returns individal car object assigned to the id
+		@repair = @car.repairs #return an array of all repair objects assigned to the car 
+		erb :list_repairs
+
 end
