6bbbf644c3adac903850629568667939b78ec5c5
Chris Philbin <chris@chrisphilbin.net>
2019-04-02 19:50:40 -0400

    Adding logic to prevent unauthenticated users from making any changes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -13,29 +13,34 @@ class CarsController < ApplicationController
 	end
 
 	post '/cars' do
+		 redirect_if_not_logged_in
 		 @car = Car.new(params[:car])
 		 @car.save
 		 redirect "/cars"
 	end
 
 	get '/cars/:id' do
+		redirect_if_not_logged_in
 		@car = Car.find_by(id: params[:id])
 		@repairs = @car.repairs
 		erb :'cars/show_car'
 	end
 
 	get '/cars/:id/add_repair' do
+		redirect_if_not_logged_in
 		@car = Car.find_by(id: params[:id])
 		@repair = Repair.new
 		erb :new_repair
 	end
 
 	get '/remove_car' do
+		redirect_if_not_logged_in
 		@cars = Car.all
 		erb :remove_car
 	end
 
 	post '/cars/:id' do
+		redirect_if_not_logged_in
 		@car = Car.find_by(id: params[:id])
 		@car.delete
 		redirect "/"
--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,24 +1,28 @@
 class RepairsController < ApplicationController
 
 	post '/repairs' do
+   		 redirect_if_not_logged_in
 		 @repair = Repair.new(params[:repair])
 		 @repair.save
 		 redirect "/cars"
 	end
 
 	get '/repairs/new' do
+		redirect_if_not_logged_in
 		@car = Car.find_by(id: params[:car_id])
 		@repair = Repair.new
 		erb :'repairs/new_repair'
 	end
 
 	get '/repairs/:id/edit' do
+		redirect_if_not_logged_in
 		@repair = Repair.find_by(id: params[:id])
 		erb :'/repairs/edit_repair'
 	end
 
 
 	post "/repairs/:id" do
+		redirect_if_not_logged_in
 		@repair = Repair.find_by(id: params[:id])
 		@repair.update(params[:repair])
 		redirect "/cars"
