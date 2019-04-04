488471c2804792509042710f144ca93ed158ac78
Chris Philbin <chris@chrisphilbin.net>
2019-04-03 22:47:55 -0400

    Cleaning up links for when user is logged out

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 	has_many :cars
 	has_secure_password
 
-	validates_uniqueness_of :username
+	validates_uniqueness_of :username, :message=>"Username has already been taken!"
 
 end
